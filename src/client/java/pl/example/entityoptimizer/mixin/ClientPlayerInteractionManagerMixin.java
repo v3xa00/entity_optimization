c400b9f17caecd5f75efbf1a27a4d7d2f182ed96
v3xa00 <tomekgregori777@gmail.com>
2026-01-02 13:14:09 +0100

    Update ClientPlayerInteractionManagerMixin.java

--- src/client/java/pl/example/entityoptimizer/mixin/ClientPlayerInteractionManagerMixin.java
+++ src/client/java/pl/example/entityoptimizer/mixin/ClientPlayerInteractionManagerMixin.java
@@ -14,13 +14,11 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.vehicle.AbstractMinecart;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.block.BannerBlock;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.WallBannerBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -68,6 +66,14 @@ public class ClientPlayerInteractionManagerMixin {
         }
     }
 
+    /**
+     * PPM z obrazem (painting) na cobwebie lub bannerze:
+     * - idziemy za tym blokiem w głąb linii wzroku (toWall), przechodząc przez kolejne cobweby/bannery,
+     * - pierwszy "normalny" blok traktujemy jako ścianę docelową i stawiamy tam obraz.
+     *
+     * Dodatkowo:
+     * - blokuje PPM na crafting table, jeśli EntityOptimizerMod.craftingDisabled == true.
+     */
     @Inject(method = "useItemOn", at = @At("HEAD"), cancellable = true)
     private void entity_optimizer$placePaintingThroughBlocks(LocalPlayer player,
                                                              InteractionHand hand,
@@ -83,7 +89,7 @@ public class ClientPlayerInteractionManagerMixin {
         BlockPos clickedPos = hit.getBlockPos();
         BlockState clickedState = minecraft.level.getBlockState(clickedPos);
 
-        // blokada craftingu sterowana flagą
+        // Blokada craftingu sterowana flagą
         if (EntityOptimizerMod.craftingDisabled && clickedState.is(Blocks.CRAFTING_TABLE)) {
             cir.setReturnValue(InteractionResult.FAIL);
             cir.cancel();
@@ -99,32 +105,46 @@ public class ClientPlayerInteractionManagerMixin {
             return;
         }
 
-        // reagujemy tylko jeśli kliknięto cobweb/banner
+        // Reagujemy tylko jeśli kliknięto cobweb lub banner
         if (!isThroughBlock(clickedState)) {
             return;
         }
 
-        // Raycast od oczu, ignorujący cobweby i bannery
-        BlockHitResult wallHit = findWallIgnoringThroughBlocks(player);
-        if (wallHit == null || wallHit.getType() != HitResult.Type.BLOCK) {
+        // Strona, na której ma wisieć obraz (ta, w którą klikasz)
+        Direction wallFace = hit.getDirection();
+        // Kierunek w głąb (od klikniętego bloku do ściany)
+        Direction toWall = wallFace.getOpposite();
+
+        BlockPos cur = clickedPos;
+        BlockPos wallPos = null;
+
+        // przechodzimy maks. 8 bloków w głąb, omijając kolejne cobweby/bannery
+        for (int i = 0; i < 8; i++) {
+            cur = cur.relative(toWall);
+            BlockState s = minecraft.level.getBlockState(cur);
+
+            if (isThroughBlock(s)) {
+                continue; // kolejny cobweb/banner – idziemy dalej
+            }
+
+            wallPos = cur;
+            break;
+        }
+
+        if (wallPos == null) {
             return;
         }
 
-        BlockPos wallPos = wallHit.getBlockPos();
         BlockState wallState = minecraft.level.getBlockState(wallPos);
-        Direction wallFace = wallHit.getDirection();
 
+        // ściana musi być solidna na tej twarzy
         if (!wallState.isFaceSturdy(minecraft.level, wallPos, wallFace)) {
             return;
         }
 
-        // udajemy kliknięcie dokładnie tej ściany
-        BlockHitResult newHit = new BlockHitResult(
-                wallHit.getLocation(),
-                wallFace,
-                wallPos,
-                false
-        );
+        // udajemy kliknięcie na środku tego bloku ściany
+        Vec3 hitVec = Vec3.atCenterOf(wallPos);
+        BlockHitResult newHit = new BlockHitResult(hitVec, wallFace, wallPos, false);
 
         entity_optimizer$placingPainting = true;
         InteractionResult result = ((MultiPlayerGameMode) (Object) this)
@@ -133,46 +153,6 @@ public class ClientPlayerInteractionManagerMixin {
 
         cir.setReturnValue(result);
         cir.cancel();
-    }
-
-    private BlockHitResult findWallIgnoringThroughBlocks(LocalPlayer player) {
-        final int MAX_STEPS = 8;
-        final double REACH = 5.0D;
-
-        Vec3 eye = player.getEyePosition(1.0F);
-        Vec3 look = player.getViewVector(1.0F);
-
-        Vec3 from = eye;
-
-        for (int i = 0; i < MAX_STEPS; i++) {
-            Vec3 to = eye.add(look.scale(REACH));
-
-            ClipContext ctx = new ClipContext(
-                    from,
-                    to,
-                    ClipContext.Block.OUTLINE,
-                    ClipContext.Fluid.NONE,
-                    player
-            );
-
-            HitResult generic = minecraft.level.clip(ctx);
-            if (generic.getType() != HitResult.Type.BLOCK) {
-                return null;
-            }
-
-            BlockHitResult res = (BlockHitResult) generic;
-            BlockPos pos = res.getBlockPos();
-            BlockState state = minecraft.level.getBlockState(pos);
-
-            if (isThroughBlock(state)) {
-                from = res.getLocation().add(look.scale(0.05D));
-                continue;
-            }
-
-            return res;
-        }
-
-        return null;
     }
 
     private static boolean isThroughBlock(BlockState state) {
